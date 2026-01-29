b59499db302b243989007c6ca078042c9cba9d5e
codealisha99 <codealisha99@gmail.com>
2026-01-29 23:01:09 +0530

    learning variables and conditionals

--- learn.java
+++ learn.java
@@ -6,20 +6,50 @@
     public static void main(String[] args) {
         System.out.println("hi lishy");
         System.out.println("******\n    *\n  *\n*    \n******");
+        int x = 5;
+        int y = 10;
+        int sum = x + y;
+        System.out.println("The sum is " + sum);
 
+        int a = 15;
+        int b = 25;
+
+        if(a==b){
+            System.out.println("a is equal to b");
+        }
+        else{
+
+            if(a>b){
+                System.out.println("a is greater than b");
+            }
+            else{
+                System.out.println("a is less than b");
+        }
+        }
+
+        Scanner scn = new Scanner(System.in);
+        int marks = scn.nextInt();
+
+        if(marks>90){
+            System.out.println("EXCELLENt!!!");
+        }
+        else 
+            if(marks>80 && marks<=90){
+                System.out.println("GOOD!!");
+            }
+        
+        else 
+            if(marks>70 && marks<=80){
+                System.out.println("FAIR!");
+            }
+        
+        else 
+            if(marks>60 && marks<=70){
+                System.out.println("MEETS EXPECTATIONS");
+            }
+        
+        else {
+            System.out.println("POOR");
+        }
     }
 }
-String s = "Java";
-String rev = "";
-
-for (int i = s.length() - 1; i >= 0; i--) {
-    rev += s.charAt(i);
-}
-System.out.println(rev);
-String s = "Java";
-String reversed = new StringBuilder(s).reverse().toString();
-System.out.println(reversed);
-String s = "madam";
-String rev = new StringBuilder(s).reverse().toString();
-
-System.out.println(s.equals(rev) ? "Palindrome" : "Not Palindrome");
