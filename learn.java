c985fcac168f9a9b096cc7f81179feff149cd0fd
codealisha99 <codealisha99@gmail.com>
2026-02-21 22:43:06 +0530

    learning operators, loops , logical operators, casting,introduction of creatin class-object

--- learn.java
+++ learn.java
@@ -1,16 +1,63 @@
- class learn{
+ class calculator{
+
+   int a;
+
+
+   public int add()
+   {
+      return 0;
+   }
+ }
+ 
+ 
+ 
+ 
+ 
+ public class learn{
         
     public static void main(String[] args) {
    
+         int num1 = 10;
+         int num2 = 20;
 
+
+         calculator cal = new calculator();
+
+         calc.add(num1, num2);
+
+
+
+
+
+
+               
       int a = 10;
+      int result = 0; 
 
-      a += 2;
-         System.out.println(a);
-         System.out.println("Hello World");
+      int result = a%2==0 ? 20 : 45;
 
+      System.out.println(result);
 
+       int i = 0;
+
+         while(true){
+         System.out.println("HELLO " + i);
+
+           // i++;
+         //}
+   
+
+         //for(int i = 0; i < 5; ++i){
+            //System.out.println("HELLO " + i);
+         //}
+
+         //for(int i = 0; i < 5; i++){
+           // System.out.println("DAY " + i);
+
+            //for(int j = 9; j<=18 ; j++){
+              //  System.out.println(j + "th HOUR");
+            //}
+         }
        
        
     }
-}
