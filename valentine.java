46d37c80a7646e56ae4681626b89e347db8bd57e
codealisha99 <codealisha99@gmail.com>
2026-03-07 23:58:25 +0530

    will you be my valentine?

---
+++ valentine.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class valentine {
+   
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValentineApplication.class, args);
+    }
+
+}
+    
+
