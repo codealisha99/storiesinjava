6c68dcc0e1eb6f63a1df44357fe74a16160d3ef0
codealisha99 <codealisha99@gmail.com>
2026-03-10 23:35:21 +0530

    array list

--- kodnest.java
+++ kodnest.java
@@ -1,12 +1,28 @@
-public class kodnest {
-    public static void main(String[] args){
-           byte a = 12; //n=8 bits the formula is -2^n-1 to +2^n-1 -1 is the formula for the range size is 1 byte=8 bits
-           short b = 124;//n=16 bits
-           int c = 45000;// n = 32 bits
-           long d = 445643;// it has 8 bytes 
-             }
+//public class kodnest {
+    //public static void main(String[] args){
+          // byte a = 12; //n=8 bits the formula is -2^n-1 to +2^n-1 -1 is the formula for the range size is 1 byte=8 bits
+           //short b = 124;//n=16 bits
+           //int c = 45000;// n = 32 bits
+           //long d = 445643;// it has 8 bytes 
+             //}
 
 
        
-    }
+    //}
 
+public class kodnest{
+  public static void greet(){
+    System.out.println("hello my love!");
+  }
+  public static void greetone(){
+    System.out.println("hello my love!");
+  }
+  public static void greettwo(){
+    System.out.println("hello my love!");
+  }
+  public static void main(String[] args){
+    greet();
+    greetone();
+    greettwo();
+  }
+}
---
+++ learning.java
@@ -0,0 +1,14 @@
+public class learning {
+    public static void main(String[] args){
+        int num[][] = new int[3][4];
+
+
+        for(int i=0;i<4;i++){
+
+
+            
+        }
+        
+
+    }
+}
