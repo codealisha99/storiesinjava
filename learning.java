b9df65390d62fa1fe8b6e7714100704d2dcd2ebc
codealisha99 <codealisha99@gmail.com>
2026-03-11 19:37:49 +0530

    learning multi-dimentional array in java

--- learning.java
+++ learning.java
@@ -1,13 +1,27 @@
 public class learning {
     public static void main(String[] args){
-        int num[][] = new int[3][4];
+        int num[][] = new int[3][];
+
+        num[0] = new int[3];
+        num[1] = new int[4];
+        num[2] = new int[2];
 
 
-        for(int i=0;i<4;i++){
+        for(int i=0;i<4;i++)
+            {
+          for(int j=0;j<4;j++){
+                
+              num[i][j] = (int)(Math.random() * 100);
+             
+            }     
+            }
 
-
-            
-        }
+            for(int n[] : num){
+                       for(int m : n){
+                        System.out.print(m + " ");
+                       }
+                       System.out.println();
+            }
         
 
     }
