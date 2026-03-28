2d679c0db44756c521ce95143b14e894df7daec2
codealisha99 <codealisha99@gmail.com>
2026-03-28 23:53:11 +0530

    adding another feature

--- snacky/snakegame.java
+++ snacky/snakegame.java
@@ -15,6 +15,38 @@ public class snakegame {
                int foodX = 3;//it is the x coordinate of the food
                int foodY = 3;//it is the y coordinate of the food
 
+               class Order {
+                int id;
+                String type; // BUY or SELL
+                int quantity;
+                double price;
+            
+                Order(int id, String type, int quantity, double price) {
+                    this.id = id;
+                    this.type = type;
+                    this.quantity = quantity;
+                    this.price = price;
+                }
+            }
+
+               ArrayList<Order> buyOrders = new ArrayList<>();
+               ArrayList<Order> sellOrders = new ArrayList<>();
+
+               for (Order buy : buyOrders) {
+                for (Order sell : sellOrders) {
+            
+                    if (buy.price >= sell.price) {
+            
+                        int tradedQty = Math.min(buy.quantity, sell.quantity);
+            
+                        System.out.println("Trade executed: " + tradedQty + " shares at " + sell.price);
+            
+                        buy.quantity -= tradedQty;
+                        sell.quantity -= tradedQty;
+                    }
+                }
+            }
+
          
     }
 }
