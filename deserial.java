865fce8aae74acc9b172ba735ef3a6a7bda3e92a
codealisha99 <codealisha99@gmail.com>
2026-07-29 02:26:33 +0530

    Serialization-Deserialization

---
+++ deserial.java
@@ -0,0 +1,18 @@
+import java.io.File;
+import java.io.ObjectInputStream;
+
+public class deserial {
+    public static void main(String[] args) throws IOException , classnotfoundexception {
+        String path = "C:\\Users\\Admin\\Desktop\\test.txt";
+        FileInputStream fis = new FileInputStream(path);
+
+        ObjectInputStream ois = new ObjectInputStream(fis);
+
+
+        Student student = (Student)ois.readObject();
+
+
+       System.err.println(student);
+
+    }
+}
