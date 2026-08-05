b16e3d77d40bfae430078c7bf6b5afa58a2a0a31
codealisha99 <codealisha99@gmail.com>
2026-08-05 23:29:36 +0530

    JDBC

---
+++ firstjdbc/demo.java
@@ -0,0 +1,14 @@
+package firstjdbc;
+
+public class demo {
+    public static void main(String[] args) {
+
+    try {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        System.out.println("Driver loaded");
+
+    } catch(Exception e){
+        e.printStackTrace();
+}
+}
+}
